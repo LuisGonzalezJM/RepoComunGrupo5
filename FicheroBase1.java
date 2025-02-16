a1aacebf9550fb803bbb49809e810a36aae65659
Luis González <134693554+LuisGonzalezJM@users.noreply.github.com>
2025-02-16 21:58:21 +0100

    Create FicheroBase1.java

---
+++ FicheroBase1.java
@@ -0,0 +1,8 @@
+//Este es el fichero original que viene creado
+//1.- Línea original de Luis
+
+public class FicheroBase1{
+    public static void main(String[] args) {
+        System.out.println("FicheroBase1");
+    }
+}
